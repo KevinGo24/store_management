8486a7febe662e551b79e6884d7accef130d4440
kevin <kevin@gmail.com>
2026-07-01 14:41:59 -0500

    subir routers

--- Assets/router/router.js
+++ Assets/router/router.js
@@ -1,2 +1,29 @@
 import { page_controller } from "../Controller/main.controller";
+// importacion  de la vista de login y su controlador
+import { view_login } from "../views/login";
+// contralador
+import controller_lgoin from "../Controller/login.controller";
+// importacion de Home
+import { views_homne } from "../views/home";
+const routes = {
+    "/": {
+        views: views_homne
+    },
+    "/login": {
+        views: view_login,
+        controller: controller_lgoin
+    }
+}
+export default function router() {
+    const master = document.getElementById("views");
 
+    const path = window.location.pathname;
+
+    const route = routes[path] || routes["/"];
+
+    master.innerHTML = route.views();
+
+    if (route.controller) {
+        route.controller();
+    }
+}
