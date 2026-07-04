43bd7664a719bfac9bd3a145998897a0fd3cceee
kevin <kevinG>
2026-07-04 09:23:25 -0500

    se agrego una capa contenedora, y un div informativo para poner al lado del login

--- Assets/views/login.js
+++ Assets/views/login.js
@@ -1,41 +1,53 @@
 export function view_login() {
     return `    
+    <div class="login_container">
+        <div class="info">
+            <img src="/Assets/picture/logo (1).png" alt="Logo Gestor Store" class="logo_2">
 
-    <form id="form_log">
-        <section class="capa_1">
-            <h2>Inicio de Sesion</h2>
-            <section class="capa_2">
-                <h1>Gestor Store</h1>
-                <label class="campo">
-                    <i class="bi bi-person"></i>
-                    <span>Rol</span>
-                </label>
-                <select id="roles" class="selection_rol">
-                    <option value="1">Admin</option>
-                    <option value="2">Usuario</option>
-                </select>
-                <label class="campo">
-                    <i class="bi bi-envelope-at-fill"></i>
-                    <span>Correo electrónico</span>
-                </label>
-                <input type="email" name="" id="correo" class="tbx">
-                <label class="campo">
-                    <i class="bi bi-lock-fill"></i>
-                    <span>Contraseña</span>
-                </label>
-                <input type="password" name="" id="pass" class="tbx">
+            <h1>Bienvenido a Gestor Store</h1>
+
+            <p>
+                Administra tus productos, usuarios y ventas desde una sola
+                plataforma de forma rápida y segura.
+            </p>
+        </div>
+        <form id="form_log">
+            <section class="capa_1">
+                <h2>Inicio de Sesion</h2>
+                <section class="capa_2">
+                    <h1>Gestor Store</h1>
+                    <label class="campo">
+                        <i class="bi bi-person"></i>
+                        <span>Rol</span>
+                    </label>
+                    <select id="roles" class="selection_rol">
+                        <option value="1">Admin</option>
+                        <option value="2">Usuario</option>
+                    </select>
+                    <label class="campo">
+                        <i class="bi bi-envelope-at-fill"></i>
+                        <span>Correo electrónico</span>
+                    </label>
+                    <input type="email" name="" id="correo" class="tbx">
+                    <label class="campo">
+                        <i class="bi bi-lock-fill"></i>
+                        <span>Contraseña</span>
+                    </label>
+                    <input type="password" name="" id="pass" class="tbx">
+                </section>
+                <button class="btn" type="submit">
+                    <i class="bi bi-box-arrow-in-right"></i>
+                    Iniciar sesión
+                </button>
+
+                <a href="#register" class="register">
+                    <i class="bi bi-person-plus-fill"></i>
+                    Registrarte
+                </a>
             </section>
-            <button class="btn" type="submit">
-                <i class="bi bi-box-arrow-in-right"></i>
-                Iniciar sesión
-            </button>
+        </form>
+    </div>
 
-            <a href="#register" class="register">
-                <i class="bi bi-person-plus-fill"></i>
-                Registrarte
-            </a>
-        </section>
-    </form>
     `
 
 }
