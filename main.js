26a0c1f1b386eaf0654cd4bcf330546ab1602130
kevin <kevin@gmail.com>
2026-07-17 13:33:50 -0500

    se modifico para capturar el #/

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // ---> importsaciones para todo los estilos
-import './Assets/Css/style.css'
+import "./Assets/Css/style.css";
 // ----> importacion del router
-import router from './Assets/router/router'
+import router from "./Assets/router/router";
 // ---> imnportar boostrap icons
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-document.addEventListener("DOMContentLoaded", router)
+document.addEventListener("DOMContentLoaded", router);
+window.addEventListener("hashchange", router);
